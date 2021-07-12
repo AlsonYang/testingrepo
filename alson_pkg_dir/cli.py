953767a5c4dd9f1c2d1153213adf5aa1e4d76a67
Alson Yang <alson.yjm@gmail.com>
2021-07-12 23:09:25 +1000

    bring back pandas

--- alson_pkg_dir/cli.py
+++ alson_pkg_dir/cli.py
@@ -6,10 +6,10 @@ import os
 def cli():
     return 'return Hello World'
     
-# def create():
-#     import pandas as pd
-#     print(os.getcwd())
-#     pd.DataFrame(columns =[1,2,3] ).to_csv('./data/test.csv', index=False)
+import pandas as pd
+def create():
+    print(os.getcwd())
+    pd.DataFrame(columns =[1,2,3] ).to_csv('./data/test.csv', index=False)
 
 if __name__ == '__main__':
     cli()
