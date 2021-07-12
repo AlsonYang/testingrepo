7d7b3f97f4b53b765aee840a5027de316020b84a
Alson Yang <alson.yjm@gmail.com>
2021-07-12 23:05:35 +1000

    commented out pandas

--- alson_pkg_dir/cli.py
+++ alson_pkg_dir/cli.py
@@ -1,16 +1,15 @@
 import click
-import pandas as pd
+
 import os
-# os.listdir('/Users/yangj2/Desktop/testingrepo/alson_pkg_dir/../data/')
-print(os.getcwd())
 
 # @click.command()
 def cli():
     return 'return Hello World'
     
-def create():
-    print(os.getcwd())
-    pd.DataFrame(columns =[1,2,3] ).to_csv('./data/test.csv', index=False)
+# def create():
+#     import pandas as pd
+#     print(os.getcwd())
+#     pd.DataFrame(columns =[1,2,3] ).to_csv('./data/test.csv', index=False)
 
 if __name__ == '__main__':
     cli()
